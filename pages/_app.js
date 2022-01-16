5f5bd8dbd5e91ba9ec2e9c80f9a1bb8142157db8
Tushar Goyal <tushar462024@gmail.com>
2022-01-16 19:16:51 +0530

    Font
    Update applicatio Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Fonts from '../components/Fonts'
 import Layout from '../components/layouts/Main'
 import theme from '../libs/theme'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
