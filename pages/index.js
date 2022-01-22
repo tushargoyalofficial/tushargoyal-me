e4e457af455ef18c5679d1fef983d94e2f3c057b
Tushar Goyal <tushar462024@gmail.com>
2022-01-23 01:31:34 +0530

    UI
    Implement page transitions with Framer motion

--- pages/index.js
+++ pages/index.js
@@ -8,117 +8,120 @@ import {
   Button
 } from '@chakra-ui/react'
 import Paragraph from '../components/Paragraph'
+import Layout from '../components/layouts/Article'
 import Section from '../components/Section'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/Bio'
 
 const Home = () => (
-  <Container>
-    <Box
-      borderRadius="lg"
-      mb={6}
-      p={3}
-      textAlign="center"
-      bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-    >
-      Hello, I&apos;m a full-stack developer based in India!
-    </Box>
-
-    <Box display={{ md: 'flex' }}>
-      <Box flexGrow={1}>
-        <Heading as="h2" variant="page-title">
-          Tushar Goyal
-        </Heading>
-        <p>Digital Craftzman ( Tech Advisor / Developer / Designer )</p>
-      </Box>
+  <Layout>
+    <Container>
       <Box
-        flexShrink={0}
-        mt={{ base: 4, md: 0 }}
-        ml={{ md: 6 }}
+        borderRadius="lg"
+        mb={6}
+        p={3}
         textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
       >
-        <Image
-          borderColor="whiteAlpha.800"
-          borderWidth={2}
-          borderStyle="solid"
-          maxWidth="100px"
-          display="inline-block"
-          borderRadius="full"
-          src="/images/tg.jpg"
-          alt="Profile image"
-        />
+        Hello, I&apos;m a full-stack developer based in India!
       </Box>
-    </Box>
 
-    <Section delay={0.1}>
-      <Heading as="h3" variant="section-title">
-        Work
-      </Heading>
-      <Paragraph>
-        Tushar is a Software Engineering professional with more than seven years
-        of experience in the field. Experienced with the latest cutting-edge
-        development tools and procedures. He has worked with several companies
-        specializing in IT services as a Software Developer, Project Manager
-        &amp; Collaborator. My Practical experience includes Web/Mobile App
-        Development, Project Management/Collaborator, Tech Advisor, Team
-        Management &amp; Consultancy.
-      </Paragraph>
-      <Box align="center" my={4}>
-        <NextLink href="/works">
-          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-            My portfolio
-          </Button>
-        </NextLink>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Tushar Goyal
+          </Heading>
+          <p>Digital Craftzman ( Tech Advisor / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/tg.jpg"
+            alt="Profile image"
+          />
+        </Box>
       </Box>
-    </Section>
-    <Section delay={0.2}>
-      <Heading as="h3" variant="section-title">
-        Bio
-      </Heading>
-      <BioSection>
-        <BioYear>1992</BioYear>
-        Born in Madhya Pradesh, India
-      </BioSection>
-      <BioSection>
-        <BioYear>2014</BioYear>
-        Completed the Bachlor&apos;s Program in college of Computer Science at
-        RGPV University, India
-      </BioSection>
-      <BioSection>
-        <BioYear>2015</BioYear>
-        Worked at ABP Group, Delhi, India
-      </BioSection>
-      <BioSection>
-        <BioYear>2017</BioYear>
-        Worked at Eulogik, MP, India
-      </BioSection>
-      <BioSection>
-        <BioYear>2018</BioYear>
-        Worked at Chapter247, MP, India
-      </BioSection>
-      <BioSection>
-        <BioYear>2020</BioYear>
-        Worked at Hiteshi Infotech, MP, India
-      </BioSection>{' '}
-      <BioSection>
-        <BioYear>2020</BioYear>
-        Worked at Protonshub Technologies, MP, India
-      </BioSection>{' '}
-      <BioSection>
-        <BioYear>2021 to present</BioYear>
-        Work as a freelance
-      </BioSection>
-    </Section>
-    <Section delay={0.3}>
-      <Heading as="h3" variant="section-title">
-        I ♥
-      </Heading>
-      <Paragraph>
-        Food, Music, Long Rides, Travel, Block-Chain, Machine Learning, Reading
-        Tech Articles, Blogs, Creating new stuffs
-      </Paragraph>
-    </Section>
-  </Container>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Tushar is a Software Engineering professional with more than seven
+          years of experience in the field. Experienced with the latest
+          cutting-edge development tools and procedures. He has worked with
+          several companies specializing in IT services as a Software Developer,
+          Project Manager &amp; Collaborator. My Practical experience includes
+          Web/Mobile App Development, Project Management/Collaborator, Tech
+          Advisor, Team Management &amp; Consultancy.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1992</BioYear>
+          Born in Madhya Pradesh, India
+        </BioSection>
+        <BioSection>
+          <BioYear>2014</BioYear>
+          Completed the Bachlor&apos;s Program in college of Computer Science at
+          RGPV University, India
+        </BioSection>
+        <BioSection>
+          <BioYear>2015</BioYear>
+          Worked at ABP Group, Delhi, India
+        </BioSection>
+        <BioSection>
+          <BioYear>2017</BioYear>
+          Worked at Eulogik, MP, India
+        </BioSection>
+        <BioSection>
+          <BioYear>2018</BioYear>
+          Worked at Chapter247, MP, India
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Worked at Hiteshi Infotech, MP, India
+        </BioSection>{' '}
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Worked at Protonshub Technologies, MP, India
+        </BioSection>{' '}
+        <BioSection>
+          <BioYear>2021 to present</BioYear>
+          Work as a freelance
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Food, Music, Long Rides, Travel, Block-Chain, Machine Learning,
+          Reading Tech Articles, Blogs, Creating new stuffs
+        </Paragraph>
+      </Section>
+    </Container>
+  </Layout>
 )
 
 export default Home
--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/Section'
 import { WorkGridItem } from '../components/GridItem'
+import Layout from '../components/layouts/Article'
 
 // Images
 import aptstudy from '../public/images/works/aptstudy.png'
@@ -11,50 +12,61 @@ import zimzim from '../public/images/works/zimzim.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem id="aptstudy" title="APT Study" thumbnail={aptstudy}>
-            A mobile application where CA aspirants can attend coaching classes,
-            purchase lectures and tests, give exams, self-assessments.
-          </WorkGridItem>
-        </Section>{' '}
-        <Section delay={0.3}>
-          <WorkGridItem id="gofundher" title="GoFundHer" thumbnail={gofundher}>
-            A website who encourage and empower women by accepting donations
-            worldwide.
-          </WorkGridItem>
-        </Section>{' '}
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="masterwizr"
-            title="MasterWizr"
-            thumbnail={masterwizr}
-          >
-            A web application where user can create and show presentations using
-            images, videos, & PDFs. User can even share it by sending a unique
-            link generated after creating a presentation.
-          </WorkGridItem>
-        </Section>{' '}
-        <Section delay={0.3}>
-          <WorkGridItem id="radlearn" title="RadLearn AI" thumbnail={radlearn}>
-            A platform where you can upload MRI/XRAYS/CITI Scans and can mark
-            annotations, as per the studies done by doctors/radiologist. This is
-            specifically made for Medical use only.
-          </WorkGridItem>
-        </Section>{' '}
-        <Section delay={0.3}>
-          <WorkGridItem id="zimzim" title="ZimZim" thumbnail={zimzim}>
-            A Mobile application similar to zoom.us where you can video
-            conference/audio conference/share screen/stream on youtube etc can
-            be done.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.3}>
+            <WorkGridItem id="aptstudy" title="APT Study" thumbnail={aptstudy}>
+              A mobile application where CA aspirants can attend coaching
+              classes, purchase lectures and tests, give exams,
+              self-assessments.
+            </WorkGridItem>
+          </Section>{' '}
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="gofundher"
+              title="GoFundHer"
+              thumbnail={gofundher}
+            >
+              A website who encourage and empower women by accepting donations
+              worldwide.
+            </WorkGridItem>
+          </Section>{' '}
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="masterwizr"
+              title="MasterWizr"
+              thumbnail={masterwizr}
+            >
+              A web application where user can create and show presentations
+              using images, videos, & PDFs. User can even share it by sending a
+              unique link generated after creating a presentation.
+            </WorkGridItem>
+          </Section>{' '}
+          <Section delay={0.3}>
+            <WorkGridItem
+              id="radlearn"
+              title="RadLearn AI"
+              thumbnail={radlearn}
+            >
+              A platform where you can upload MRI/XRAYS/CITI Scans and can mark
+              annotations, as per the studies done by doctors/radiologist. This
+              is specifically made for Medical use only.
+            </WorkGridItem>
+          </Section>{' '}
+          <Section delay={0.3}>
+            <WorkGridItem id="zimzim" title="ZimZim" thumbnail={zimzim}>
+              A Mobile application similar to zoom.us where you can video
+              conference/audio conference/share screen/stream on youtube etc can
+              be done.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
